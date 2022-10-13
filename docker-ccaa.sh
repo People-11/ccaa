3a9895e5a4c40540d427c1426217fb098c84061f
spb512 <spb512@163.com>
2022-10-13 13:33:51 +0800

    1、修改路径名称。

--- docker-ccaa.sh
+++ docker-ccaa.sh
@@ -10,7 +10,7 @@ export PATH
 #各种路径设置
 aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2'
 filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.0.16/linux-amd64-filebrowser.tar.gz'
-master_url='https://github.com/helloxz/ccaa/archive/master.zip'
+master_url='https://github.com/spb512/ccaa/archive/master.zip'
 ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web'
 
 #安装前的检查
@@ -133,7 +133,7 @@ function cleanup(){
 
 #卸载
 function uninstall(){
-	wget -O ccaa-uninstall.sh https://raw.githubusercontent.com/helloxz/ccaa/master/uninstall.sh
+	wget -O ccaa-uninstall.sh https://raw.githubusercontent.com/spb512/ccaa/master/uninstall.sh
 	sh ccaa-uninstall.sh
 }
 
