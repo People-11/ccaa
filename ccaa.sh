4c89c1d7f5fab6707c446a2bc0d5df9c10b5586d
spb512 <spb512@163.com>
2022-11-10 20:53:41 +0800

    Create ccaa.sh
    1、升级filebrowser版本。

--- ccaa.sh
+++ ccaa.sh
@@ -16,7 +16,7 @@ if [ $1 = 'cdn' ]
 	ccaa_web_url='http://soft.xiaoz.top/linux/ccaa_web.tar.gz'
 	else
 	aria2_url='https://github.com/q3aql/aria2-static-builds/releases/download/v1.36.0/aria2-1.36.0-linux-gnu-64bit-build1.tar.bz2'
-	filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.22.4/linux-amd64-filebrowser.tar.gz'
+	filebrowser_url='https://github.com/filebrowser/filebrowser/releases/download/v2.23.0/linux-amd64-filebrowser.tar.gz'
 	master_url='https://github.com/spb512/ccaa/archive/master.zip'
 	ccaa_web_url='http://soft.xiaoz.org/linux/ccaa_web.tar.gz'
 fi
